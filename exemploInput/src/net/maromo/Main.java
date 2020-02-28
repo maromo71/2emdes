9a3ccea43b8ab5be2a31e1b443e831d85fedc94e
maromo71 <professormoraes@gmail.com>
2020-02-28 14:02:39 -0300

    Aula do Maromo e do Rodrigo em 28 de fev

---
+++ exemploInput/src/net/maromo/Main.java
@@ -0,0 +1,22 @@
+package net.maromo;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    String nome, invertido = "";
+	    //Para a entrada usamos Scanner
+        Scanner input = new Scanner(System.in);
+        System.out.println("Digite o nome do cara: ");
+        nome = input.nextLine();
+        System.out.println("Nome digitado: " + nome);
+        int fim, tam;
+        tam = nome.length(); //Retornar o tamanho
+        fim = tam - 1;
+        for (int i = fim; i >= 0; i--) {
+            invertido += nome.charAt(i);
+        }
+        System.out.println("Nome invertido: " + invertido); 
+    }
+}
